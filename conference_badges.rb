e3f270af3d7a3e826cafa9c27f91a036e153b0e2
Benjamin Jones <bljones36@gmail.com>
2018-07-24 02:00:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
+end
+
+def assign_rooms(speaker)
+x = 0
+  while x < 7
+  "Hello, #{speaker[x]}! You'll be assigned to room #{x+1}!"
+end
+
+def batch_badge_creator(array)
+end
+
+def printer
+  puts ""
+end
